5563257ae90995d160ced1d59213721303bf2a34
Marina Ferreira <marina.ferreira.dev@gmail.com>
2019-12-01 16:13:21 -0300

    Homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './pages/HomePage/HomePage';
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HomePage.scss'
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Title</h1>
+          <span className="subtitle">Lorem ipsum</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage
