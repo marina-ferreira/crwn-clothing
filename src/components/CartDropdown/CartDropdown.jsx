374822e7a613afad56ff3a76d85a2277e6843225
Marina Ferreira <marina.ferreira.dev@gmail.com>
2019-12-22 22:07:56 -0300

    Adds counter to shopping bag icon

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Button from '../Button/Button'
+import CartItem from '../CartItem/CartItem'
 
 import './CartDropdown.sass'
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map(item => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
 
     <Button>GO TO CHECKOUT</Button>
   </div>
 )
 
-export default CartDropdown
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)
--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -5,15 +5,19 @@ import { toggleCart } from '../../redux/cart/cart.actions'
 
 import './CartIcon.sass'
 
-const CartIcon = ({ toggleCart }) => (
+const CartIcon = ({ toggleCart, itemCount }) => (
   <div className="cart-icon" onClick={toggleCart}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+<span className="item-count">{itemCount}</span>
   </div>
 )
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((total, item) => (total += item.quantity), 0)
+})
 
 const mapDispatchToPRops = dispatch => ({
   toggleCart: () => dispatch(toggleCart())
 })
 
-export default connect(null, mapDispatchToPRops)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToPRops)(CartIcon)
