4184990c60ed88b7ee27645cf069225ecbe21287
Marina Ferreira <marina.ferreira.dev@gmail.com>
2019-12-23 22:00:34 -0300

    Hide dropdown when navigation to checkout

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -6,10 +6,11 @@ import { withRouter } from 'react-router-dom'
 import Button from '../Button/Button'
 import CartItem from '../CartItem/CartItem'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCart } from '../../redux/cart/cart.actions'
 
 import './CartDropdown.sass'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ?
@@ -20,7 +21,12 @@ const CartDropdown = ({ cartItems, history }) => (
       }
     </div>
 
-    <Button onClick={() => history.push('/checkout')}>GO TO CHECKOUT</Button>
+    <Button onClick={() => {
+      history.push('/checkout')
+      dispatch(toggleCart())
+    }}>
+      GO TO CHECKOUT
+    </Button>
   </div>
 )
 
