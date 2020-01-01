572cab6a16b3b6a82049713df24e0de5a5e573af
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-01-01 12:02:08 -0300

    Shop hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
 import { selectCollection } from 'redux/shop/shop.selectors'
 import CollectionItem from 'components/collection-item/collection-item.component'
+
 import './collection.styles.sass'
 
 const CollectionPage = ({ collection }) => {
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
@@ -8,34 +8,28 @@ import CollectionPageContainer from 'pages/collection/collection.container'
 
 import './shop.styles.sass'
 
-class ShopPage extends React.Component {
-  unsubscribeFromSnapshot = null
-
-  componentDidMount() {
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart()
-  }
+  }, [fetchCollectionsStart])
 
-  render() {
-    const { match } = this.props
-
-    return (
-      <div className="shop-page">
-        <Route
-          exact
-          path={match.path}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
-        />
-      </div>
-    )
-  }
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={match.path}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStart: dispatch(fetchCollectionsStart())
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage)
