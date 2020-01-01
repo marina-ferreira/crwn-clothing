1db050978910e79114ef1e8dfcab1fe4e4bc75d5
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-01-01 09:46:21 -0300

    Global shop sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { takeLatest, call, put, all } from 'redux-saga/effects'
 import { firestore, convertSnapshotToMap } from 'firebase/firebase.utils'
 import {
   fetchCollectionsSuccess,
@@ -23,4 +23,8 @@ export function* fetchCollectionsStart() {
     ShopTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   )
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)])
 }
