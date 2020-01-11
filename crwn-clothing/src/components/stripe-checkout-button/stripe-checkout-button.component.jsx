50173e2d15cf01fb0fb9a6a92e68b9d9fdeb7509
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-01-11 11:43:51 -0300

    Sends payment request to stripe

--- crwn-clothing/src/components/stripe-checkout-button/stripe-checkout-button.component.jsx
+++ crwn-clothing/src/components/stripe-checkout-button/stripe-checkout-button.component.jsx
@@ -1,15 +1,31 @@
 
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 import './stripe-checkout-button.styles.sass'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceInCents = price * 100
   const publishableKey = 'pk_test_V29aTZS3XMtw4CrsR9UfxEa800ocT1hG0f'
-  const onToken = (token) => {
-    console.log(token)
-    alert('Payment successful.')
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceInCents,
+        token
+      }
+    })
+    .then(response => alert('Payment successful'))
+    .catch(error => {
+      console.log('Payment error:', JSON.parse(error))
+
+      alert(
+      `There was an issue with you payment.
+      Please make sure you use the privided credit card`)
+    })
   }
 
   return (
