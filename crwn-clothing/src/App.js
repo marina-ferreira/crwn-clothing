3297736cbb26307b329c7ccf9167bf8a95f3e205
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-02-01 11:21:28 -0300

    Performance: Error boundary

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, lazy, Suspense } from 'react'
 import Header from './components/header/header.component'
+import ErrorBoundary from './components/error-boundary/error-boundary.component'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
@@ -21,19 +22,20 @@ const App = ({ currentUser, checkUserSession }) => {
   return (
     <div>
       <Header />
-
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route
-            exact
-            path='/auth'
-            render={() => currentUser ? (<Redirect to='/' />) : (<AuthPage />)}
-          />
-        </Switch>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route exact path='/checkout' component={CheckoutPage} />
+            <Route
+              exact
+              path='/auth'
+              render={() => currentUser ? (<Redirect to='/' />) : (<AuthPage />)}
+            />
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   )
 }
